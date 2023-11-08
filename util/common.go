70b7455f4e248b5cdeffe52500e6e5ecdd3cf6cd
teq-quangnguyen <quangnguyen@teqnological.asia>
2023-11-08 10:59:06 +0700

    test 01

--- util/common.go
+++ util/common.go
@@ -1,5 +1,15 @@
 package util
 
+import (
+	"math/rand"
+	"time"
+)
+
 func IToPI(i int64) *int64 {
 	return &i
+}
+
+func RandomTest(f, t int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(t-f+1) + f
 }
